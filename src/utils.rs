4e352dc71ea8f673fb7b880d3ea61b3b07bbdf19
Croxx <mrcroxx@outlook.com>
2025-01-10 17:58:09 +0800

    chore: fullfill crate info (#1)
    * chore: fullfill crate info
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>
    
    * chore: fix license header, remove unused files
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>
    
    * chore: tiny refactor
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>
    
    ---------
    
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,17 @@
+// Copyright 2025 mixtrics Project Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /// Scoped functional programming extensions.
 pub trait Scope {
     /// Scoped with ownership.
@@ -9,8 +23,8 @@ pub trait Scope {
         f(self)
     }
 
-    #[expect(unused)]
     /// Scoped with reference.
+    #[allow(unused)]
     fn with_ref<F, R>(&self, f: F) -> R
     where
         F: FnOnce(&Self) -> R,
@@ -18,8 +32,8 @@ pub trait Scope {
         f(self)
     }
 
-    #[expect(unused)]
     /// Scoped with mutable reference.
+    #[allow(unused)]
     fn with_mut<F, R>(&mut self, f: F) -> R
     where
         F: FnOnce(&mut Self) -> R,

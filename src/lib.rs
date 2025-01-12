259eed5d88f7b777785282b7015ffd4123360513
Croxx <mrcroxx@outlook.com>
2025-01-12 15:21:09 +0800

    doc: display feature flag on rust docs (#9)
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 mod utils;
 
 pub mod metrics;

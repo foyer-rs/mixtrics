e02206f2c3711e5546e3a0787cdf2ebb8679fcd1
tison <wander4096@gmail.com>
2026-06-02 14:59:08 +0800

    feat: support opentelemetry-rust 0.32 (#36)
    Signed-off-by: tison <wander4096@gmail.com>

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -23,6 +23,7 @@ pub use opentelemetry_0_28;
 pub use opentelemetry_0_29;
 pub use opentelemetry_0_30;
 pub use opentelemetry_0_31;
+pub use opentelemetry_0_32;
 pub use prometheus_0_13;
 pub use prometheus_0_14;
 pub use prometheus_client_0_22;
@@ -152,6 +153,16 @@ macro_rules! test {
             use $crate::{
                 metrics::BoxedRegistry, registry::opentelemetry_0_31::OpenTelemetryMetricsRegistry,
                 test_utils::opentelemetry_0_31::global::meter,
+            };
+            let registry: BoxedRegistry = Box::new(OpenTelemetryMetricsRegistry::new(meter("test")));
+            ($f)(&registry);
+        }
+
+        #[test]
+        fn test_opentelemetry_0_32() {
+            use $crate::{
+                metrics::BoxedRegistry, registry::opentelemetry_0_32::OpenTelemetryMetricsRegistry,
+                test_utils::opentelemetry_0_32::global::meter,
             };
             let registry: BoxedRegistry = Box::new(OpenTelemetryMetricsRegistry::new(meter("test")));
             ($f)(&registry);
